a77113c7ec8a47d8dc7bf0b726b65fec20cb83aa
Thomas Chambon <thomas@mywittygames.com>
2012-03-12 13:21:30 +0100

    commit 6

---
+++ acheter.php
@@ -0,0 +1,54 @@
+﻿<!DOCTYPE html>
+<html>
+
+
+<head>
+
+	<link rel="stylesheet" href="defaut.css">
+	<link rel="stylesheet" href="acheter.css">
+
+<meta charset="UTF-8"/>	
+	
+<title>Acheter Ice3</title>
+
+
+</head>
+
+<body>
+
+	<div id="conteneur">
+		<div id="header">
+
+			<img src="img/bandeau_header.png"/>
+
+		</div>
+
+		<nav>
+			<a class="bleu" href="index.html">Le jeu</a>
+			<a class="bleu" href="/ice3/acheter.html">Où l'acheter</a>
+			<a class="bleu" href="/ice3/presse.html">La presse</a>
+			<a class="bleu" href="/ice3/contact.html">Contact</a>
+		</nav>
+		
+		<section id="contenu">
+		
+			<section id="achat_boutique">
+				<p>Retrouvez Ice3 sur la boutique en ligne de mywittygames.com</p>
+				<img class="img_description" src="img/description_200.jpg"/>
+				<a class="link_jenveuxun" href="http://www.mywittygames.com/shop/product/ice3" target="_blank">Je le veux! &raquo;</a>
+				<p>Vous pouvez aussi retrouver Ice3 chez nos partenaires</p>
+				<img class="partenaires" src="img/Partenaires.jpg"/>
+			</section>
+			
+		</section>
+		
+		<footer>
+			
+		</footer>
+
+	</div>
+</body>
+
+</html>
+
+
---
+++ presse.php
@@ -0,0 +1,47 @@
+﻿<!DOCTYPE html>
+<html>
+
+
+<head>
+
+	<link rel="stylesheet" href="defaut.css">
+	<link rel="stylesheet" href="acheter.css">
+
+<meta charset="UTF-8"/>	
+	
+<title>Presse Ice3</title>
+
+
+</head>
+
+<body>
+
+	<div id="conteneur">
+		<div id="header">
+
+			<img src="img/bandeau_header.png"/>
+
+		</div>
+
+		<nav>
+			<a class="bleu" href="index.html">Le jeu</a>
+			<a class="bleu" href="/ice3/acheter.html">Où l'acheter</a>
+			<a class="bleu" href="/ice3/presse.html">La presse</a>
+			<a class="bleu" href="/ice3/contact.html">Contact</a>
+		</nav>
+		
+		<section id="contenu">
+
+			<h1>Ils parlent de Ice3 :</h1>			
+		</section>
+		
+		<footer>
+			
+		</footer>
+
+	</div>
+</body>
+
+</html>
+
+
