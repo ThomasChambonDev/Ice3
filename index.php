457f06d0b35e70e90d56e11cd5b4bb5edec1ef1a
Thomas Chambon <thomas@mywittygames.com>
2012-03-27 19:15:52 +0200

    google analytics

--- index.php
+++ index.php
@@ -5,12 +5,27 @@
 <head>
 
 	<link rel="stylesheet" href="defaut.css">
-	<link rel="stylesheet" href="jeu.css">
-
+	<link rel="stylesheet" href="jeu.css">	
+	
+	
 <meta charset="UTF-8"/>	
 	
 <title>Ice3, le jeu le plus givré de l'année</title>
 
+	<script type="text/javascript">
+
+		//Script pour google analytics
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-17906341-2']);
+		_gaq.push(['_trackPageview']);
+
+		(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+
+	</script>
 
 </head>
 
